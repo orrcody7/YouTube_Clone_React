30e4af3dc8c1e1c3709134e0583df89901e3853b
unknown <orrcody7@gmail.com>
2021-09-17 10:12:54 -0600

    created the index.js and app.jsx files, verified that the 'root' is connected to the html file

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import React, { Component, component } from 'react';
+import axios from 'axios';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+
+
+
+    render() { 
+        return ( 
+            <div>
+                <h1>Hello World!</h1>
+            </div>
+         );
+    }
+}
+ 
+export default App;
